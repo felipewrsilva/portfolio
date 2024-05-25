1a5fe969d48ef4c8b9f7bf83f855b258903bef76
Felipe Silva <78854294+felipewrsilva@users.noreply.github.com>
2024-05-25 03:12:29 -0300

    add magic button to hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { FaLocationArrow } from 'react-icons/fa6'
+
+import MagicButton from './MagicButton'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
@@ -15,26 +17,38 @@ const Hero = () => {
           fill="purple"
         />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div
+        className="absolute left-0 top-0 flex h-screen w-full
+       items-center justify-center bg-white bg-grid-black-100/[0.2] dark:bg-black-100 dark:bg-grid-white/[0.03]"
+      >
+        <div
+          className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white
+         [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"
+        />
+      </div>
 
-        <div className="absolute left-0 top-0 flex  h-screen w-full items-center justify-center bg-white bg-grid-black/[0.2] dark:bg-black-100 dark:bg-grid-white/[0.05]">
-          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
-        </div>
+      <div className="relative z-10 my-20 flex justify-center">
+        <div className="flex max-w-[89vw] flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
+          <p className="max-w-80 text-center text-xs uppercase tracking-widest text-blue-100">
+            Dynamic Web Magic with Next.js
+          </p>
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
 
-        <div className="relative z-10 my-20 flex justify-center">
-          <div className="flex max-w-[89vw] flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
-            <h2 className="max-w-80 text-center text-xs uppercase tracking-widest text-blue-100">
-              Dynamic Web Magic with Next.js
-            </h2>
+          <p className="mb-4 text-center text-sm md:text-lg md:tracking-wider lg:text-2xl">
+            Hi! I&apos;m Felipe, a Next.js Developer based in Brazil.
+          </p>
 
-            <TextGenerateEffect
-              className="text-center text-[40px] md:text-5xl lg:text-6xl"
-              words="Transforming Concepts into Seamless Experiences"
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
             />
-
-            <p className="mb-4 text-center md:text-lg md:tracking-wider lg:text-2xl">
-              Hi, I&apos;m Felipe, a Next.js Developer based in Brazil.
-            </p>
-          </div>
+          </a>
         </div>
       </div>
     </div>
