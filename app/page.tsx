a4d4748d70a15a0d0bff1097a86d0484eb6d5ede
Felipe Silva <78854294+felipewrsilva@users.noreply.github.com>
2024-05-25 02:58:29 -0300

    add text generate effect to hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 h-screen flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <h1 className="text-white">Hello portfolio!</h1>
+    <main className="relative mx-auto flex h-screen flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
+      <div className="w-full max-w-7xl">
+        <h1 className="text-yellow">Hello portfolio!</h1>
         <Hero />
       </div>
     </main>
