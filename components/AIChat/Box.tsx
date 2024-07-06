da6d2720f7ab35dc1be714a2f288ef579d0e9de2
Felipe Silva <78854294+felipewrsilva@users.noreply.github.com>
2024-07-06 19:50:39 -0300

    feat: Add AIChat components for chat functionality

---
+++ components/AIChat/Box.tsx
@@ -0,0 +1,69 @@
+import { cn } from '@/lib/utils'
+import { useChat } from 'ai/react'
+import { useEffect, useRef } from 'react'
+import CloseButton from '@/components/AIChat/CloseButton'
+import AIChatInput from '@/components/AIChat/Input'
+import AIChatMessageBody from '@/components/AIChat/MessageBody'
+
+interface AIChatBoxProps {
+  open: boolean
+  onClose: () => void
+}
+
+const AIChatBox = ({ open, onClose }: AIChatBoxProps) => {
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    setMessages,
+    isLoading,
+    error,
+  } = useChat()
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  const scrollRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+    }
+  }, [messages])
+
+  useEffect(() => {
+    if (open) {
+      inputRef.current?.focus()
+    }
+  }, [open])
+
+  const lastMessageIsUser = messages[messages.length - 1]?.role === 'user'
+
+  return (
+    <div
+      className={cn(
+        'fixed bottom-10 right-2 top-2 z-50 max-w-md p-1',
+        open ? 'block' : 'hidden',
+      )}
+    >
+      <CloseButton onClose={onClose} />
+      <div className="flex h-full flex-col overflow-hidden rounded border bg-background shadow-xl">
+        <AIChatMessageBody
+          messages={messages}
+          isLoading={isLoading}
+          error={error}
+          scrollRef={scrollRef}
+          lastMessageIsUser={lastMessageIsUser}
+        />
+        <AIChatInput
+          handleSubmit={handleSubmit}
+          setMessages={setMessages}
+          input={input}
+          handleInputChange={handleInputChange}
+          inputRef={inputRef}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default AIChatBox
---
+++ components/AIChat/Input.tsx
@@ -0,0 +1,49 @@
+import { Trash, SendHorizontal } from 'lucide-react'
+import { Message } from 'ai/react'
+import React from 'react'
+
+interface AIChatSentProps {
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+  setMessages: (messages: Message[]) => void
+  input: string
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  inputRef: React.RefObject<HTMLInputElement>
+}
+
+const AIChatInput = ({
+  handleSubmit,
+  setMessages,
+  input,
+  handleInputChange,
+  inputRef,
+}: AIChatSentProps) => {
+  return (
+    <form onSubmit={handleSubmit} className="m-3 flex gap-1">
+      <button
+        type="button"
+        className="flex w-10 flex-none items-center justify-center"
+        title="Clear chat"
+        onClick={() => setMessages([])}
+      >
+        <Trash size={24} />
+      </button>
+      <input
+        value={input}
+        onChange={handleInputChange}
+        placeholder="Say something..."
+        className="grow rounded border bg-background px-3 py-2"
+        ref={inputRef}
+      />
+      <button
+        type="submit"
+        className="flex w-10 flex-none items-center justify-center disabled:opacity-50"
+        disabled={input.length === 0}
+        title="Submit message"
+      >
+        <SendHorizontal size={24} />
+      </button>
+    </form>
+  )
+}
+
+export default AIChatInput
