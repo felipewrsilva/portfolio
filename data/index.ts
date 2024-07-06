1064819672aa03d0dc0e43ad147f4292d159d84e
Felipe Silva <78854294+felipewrsilva@users.noreply.github.com>
2024-07-06 19:47:53 -0300

    chore: Update English dictionary translations and improve user experience

--- data/index.ts
+++ data/index.ts
@@ -53,14 +53,14 @@ export interface Dictionary {
 export const defaultEnglishDictionary: Dictionary = {
   page: {
     experience: 'Experience',
-    hero: 'Hero',
+    hero: 'Show my work',
     client: {
-      words: 'Words',
-      partners: 'Partners',
+      words: 'Kind words from',
+      partners: 'satisfied partners',
     },
     bentoGrid: {
-      copied: 'Copied',
-      notCopied: 'Not Copied',
+      copied: 'Email is copied!',
+      notCopied: 'Copy my email address',
     },
     floatingNav: {
       about: 'About',
@@ -68,37 +68,128 @@ export const defaultEnglishDictionary: Dictionary = {
       contact: 'Contact',
     },
     header: {
-      title: 'Title',
-      tagline: 'Tagline',
-      introduction: 'Introduction',
-      callToAction: 'Call to Action',
+      title: 'Dynamic Web Magic with Next.js',
+      tagline: 'Transforming Concepts into Seamless User Experiences',
+      introduction: "Hi! I'm Felipe, a Fullstack Developer based in Brazil.",
+      callToAction: 'Show my work',
     },
     gridItems: [
-      { title: 'Title', description: 'Description' },
-      { title: 'Title', description: 'Description' },
-      { title: 'Title', description: 'Description' },
-      { title: 'Title', description: 'Description' },
-      { title: 'Title', description: 'Description' },
-      { title: 'Title', description: 'Description' },
+      {
+        title:
+          'I prioritize client collaboration, fostering open communication',
+        description: '',
+      },
+      {
+        title: "I'm very flexible with time zone communications",
+        description: '',
+      },
+      {
+        title: 'My tech stack',
+        description: 'I constantly try to improve things',
+      },
+      {
+        title: 'Tech enthusiast with a passion for development',
+        description: '',
+      },
+      {
+        title: 'Currently working on CI/CD library for software deployments',
+        description: 'The Inside Scoop',
+      },
+      { title: 'Do you want to start a project together?', description: '' },
     ],
-    projects: [],
-    testimonials: [],
+    projects: [
+      {
+        title: '3D Solar System Planets to Explore',
+        description:
+          'Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.',
+      },
+      {
+        title: 'Yoom - Video Conferencing App',
+        description:
+          'Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.',
+      },
+      {
+        title: 'AI Image SaaS - Canva Application',
+        description:
+          'A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.',
+      },
+      {
+        title: 'Animated Apple iPhone 3D Website',
+        description:
+          'Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects.',
+      },
+    ],
+    testimonials: [
+      {
+        quote:
+          'At UniChem, Felipe was instrumental in ensuring data privacy and compliance with relevant regulations. They proactively identified and mitigated critical issues early in the development process, preventing a potential 30% increase in post-release defects. Their ability to develop comprehensive test plans and address issues before they became major problems highlights their forward-thinking and meticulous approach to software development.',
+        name: 'Carlos Mendes',
+        role: 'Quality Assurance Lead',
+      },
+      {
+        quote:
+          'During their tenure at UniChem, Felipe excelled in collaborative environments, aligning technical solutions with business goals by working closely with product managers and stakeholders. Their leadership in knowledge-sharing sessions and facilitation of team learning fostered a culture of continuous improvement and innovation within the team.',
+        name: 'Pedro Santos',
+        role: 'Product Development Manager',
+      },
+      {
+        quote:
+          'At Sollana, Felipe demonstrated exceptional skills in generating data-driven insights that directly supported key business decisions and boosted revenue. Their proficiency in developing and maintaining SQL scripts, optimizing database performance, and reducing user-reported defects by 30% through automated web application testing with Selenium, underscores their commitment to delivering high-quality solutions.',
+        name: 'Bruno Almeida',
+        role: 'Data Analytics Director',
+      },
+      {
+        quote:
+          'In their role at IQVia, Felipe significantly enhanced operational efficiency by independently automating database deployments across eight Latin American countries. Their implementation of CI/CD pipelines and automated testing not only improved software quality but also accelerated development cycles, showcasing their strong capability in streamlining processes.',
+        name: 'Mariano Pereira',
+        role: 'IT Operations Manager',
+      },
+    ],
     experiences: [
-      { title: 'Title', description: 'Description' },
-      { title: 'Title', description: 'Description' },
-      { title: 'Title', description: 'Description' },
-      { title: 'Title', description: 'Description' },
+      {
+        title: 'Software Engineer Intern',
+        description:
+          'Assisted in the development of a web-based platform, enhancing functionality and interactivity.',
+      },
+      {
+        title: 'Mobile App Dev - Sollana Tech',
+        description:
+          'Designed and developed mobile app for both iOS & Android platforms using React Native.',
+      },
+      {
+        title: 'Freelance App Dev Project',
+        description:
+          'Led the dev of a mobile app for a client, from initial concept to deployment on app stores.',
+      },
+      {
+        title: 'Lead Fullstack Developer',
+        description:
+          'Developed and maintained features using modern frontend and backend technologies.',
+      },
     ],
-    approach: 'Approach',
+    approach: 'My approach',
     approaches: [
-      { title: 'Title', description: 'Description' },
-      { title: 'Title', description: 'Description' },
-      { title: 'Title', description: 'Description' },
+      {
+        title: 'Planning & Strategy',
+        description:
+          "We'll collaborate to map out your website's goals, target audience, and key functionalities. We'll discuss things like site structure, navigation, and content requirements.",
+      },
+      {
+        title: 'Development & Progress Update',
+        description:
+          'Once we agree on the plan, I cue my lofi playlist and dive into coding. From initial sketches to polished code, I keep you updated every step of the way.',
+      },
+      {
+        title: 'Development & Launch',
+        description:
+          "This is where the magic happens! Based on the approved design, I'll translate everything into functional code, building your website from the ground up.",
+      },
     ],
     footer: {
-      presence: 'Presence',
-      discussion: 'Discussion',
-      contact: 'Contact',
+      presence: 'Ready to take your digital presence to the next level?',
+      discussion:
+        "Reach out to me today and let's discuss how I can help you achieve your goals.",
+      contact: "Let's get in touch",
     },
   },
 }
