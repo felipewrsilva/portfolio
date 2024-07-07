1d34581efafb06520c023dc8a9a98110018b8b3d
Felipe Silva <78854294+felipewrsilva@users.noreply.github.com>
2024-07-07 00:21:21 -0300

    chore: Update dependencies and add AIChat components for chat functionality

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,17 +10,7 @@ export const runtime = 'edge'
 const formatMessage = (message: VercelChatMessage) => {
   return `${message.role}: ${message.content}`
 }
-const TEMPLATE = `You are a helpful AI assistant that will help recruiters know my skills and experience better. I will provide you with the information you need to answer their questions.
-
-Felipe Silva is an experienced Senior Full Stack Developer based in São Paulo, Brazil. He is passionate about empowering businesses through robust technology solutions. He has a proven track record of success in automating deployments, creating CI/CD pipelines, and architecting scalable cloud projects. 
-
-Felipe currently works at IQVia, where he has achieved significant milestones such as reducing deployment time by 40% and improving software quality by 50%. His previous experiences at Sollana and UniChem highlight his ability to develop reliable full-stack applications, generate valuable insights from databases, and optimize system performance.
-
-He is proficient in a wide range of technologies including .Net Core/C#, React, Node.js, Git, Docker, SQL Server, MySQL, Azure, and more. His personal projects demonstrate his capability to develop secure financial platforms and modern e-commerce solutions.
-
-Felipe's collaborative efforts and ability to design scalable backend systems have consistently contributed to successful project outcomes. He is fluent in English and Portuguese.
-
-For more details, contact him via email at felipewrsilva@gmail.com.
+const TEMPLATE = `You are an AI assistant.
 
 Current conversation:
 {chat_history}
@@ -40,7 +30,7 @@ export async function POST(req: NextRequest) {
     const model = new ChatOpenAI({
       temperature: 0.8,
       model: 'gpt-3.5-turbo-0125',
-      maxTokens: 10,
+      maxTokens: 100,
     })
 
     const outputParser = new HttpResponseOutputParser()
